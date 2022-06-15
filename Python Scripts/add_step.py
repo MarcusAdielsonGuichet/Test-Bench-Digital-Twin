ffad364b130ecaa9f827733581a202dd6f5858d5
MarcusAdielsonGuichet <106607019+MarcusAdielsonGuichet@users.noreply.github.com>
2022-06-15 10:11:18 +0200

    Create add_step.py
    Add a single step to an inp file

---
+++ Python Scripts/add_step.py
@@ -0,0 +1,26 @@
+def add_step(inp_file):
+  previous_inp=open(inp_file, 'r')
+  new_inp=open("new_inp_file.inp", 'w')
+  for line in previous_inp:
+    if "*END STEP" in line:
+			end_step_count+=1
+		if end_step_count==nb-1:
+				new_inp.write("*STEP, NLGEOM, INC=1000000\n")
+	      new_inp.write("*DYNAMIC\n")
+        new_inp.write(str(first_increment_value)+","+str(step_duration)+","+str(min_increment_value)+","+str(max_increment_value)+"\n")
+        
+        #Displaced nodes
+        new_inp.write("*BOUNDARY\n")
+        new_inp.write("ConstraintDisplacement,"+str(first_degree_freedom)+","+str(last_degree_freedom)+","+str(disp_value)+"\n")
+        
+        #Fixed nodes
+        new_inp.write("*BOUNDARY\n")
+        new_inp.write("ConstraintDisplacement,1,6,0\n")
+        
+        #Output
+        new_inp.write("*NODE PRINT, NSET=ConstraintDisplacement\n")
+        new_inp.write("U")
+        new_inp.write("*END STEP")
+    new_inp.write(line)
+  new_inp.close()
+  previous_inp.close()
