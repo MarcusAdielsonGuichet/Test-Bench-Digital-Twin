6f3743fa7e744b5441667bd399a9d99afe844d55
MarcusAdielsonGuichet <106607019+MarcusAdielsonGuichet@users.noreply.github.com>
2022-06-16 16:48:02 +0200

    Update run.py

--- Python Scripts/run.py
+++ Python Scripts/run.py
@@ -2,4 +2,5 @@ import os
 
 def run(ccx_exe_path,inp_path):
   os.system('cd '+ccx_exe_path)
+  inp_file_name=os.path.splitext(os.path.basename(file))[0]#not
   os.system('cmd /k ccx '+inp_file_name)
