2e2232eb124cbf6c11c506b6371f8aaa7f2ba0c8
MarcusAdielsonGuichet <106607019+MarcusAdielsonGuichet@users.noreply.github.com>
2022-06-23 10:17:48 +0200

    Update run.py

--- Python Scripts/run.py
+++ Python Scripts/run.py
@@ -1,6 +1,11 @@
 import os
-
-def run(ccx_exe_path,inp_path):
-  os.system('cd '+ccx_exe_path)
-  inp_file_name=os.path.splitext(os.path.basename(file))[0]#not
-  os.system('cmd /k ccx '+inp_file_name)
+#The first inp step file is written by hand, later iterations will use the gmesh converter directly.
+def run(ccx_exe_path,inp_directory):
+  os.system('cmd /k cd '+inp_directory)
+  dir_path = inp_directory
+  for root, dirs, files in os.walk(dir_path):
+    for file in files:
+      if file.endswith('.inp'):
+        inp_file_name=os.path.splitext(os.path.basename(file))[0] #ccx only needs the filename, not the extension
+  os.system('cmd /k '+ccx_exe_path+" "+inp_file_name)
+  
