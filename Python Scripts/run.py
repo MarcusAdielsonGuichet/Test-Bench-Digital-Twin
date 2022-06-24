69c0b3cf5e30d67e85e4688dfd3f8954e1a0ecef
MarcusAdielsonGuichet <106607019+MarcusAdielsonGuichet@users.noreply.github.com>
2022-06-24 11:15:43 +0200

    Update run.py
    Work on the iterative steps

--- Python Scripts/run.py
+++ Python Scripts/run.py
@@ -4,22 +4,61 @@ import shutil
 import glob
 
 #The first inp step file is written by hand, later iterations will use the gmesh converter directly.
-def run_first_step(ccx_exe_path,inp_directory):
-  os.chdir(inp_directory)
-  dir_path = inp_directory
-  for root, dirs, files in os.walk(dir_path):
+def run(ccx_exe_path,work_dir,first_inp_directory):
+  os.chdir(first_inp_directory)
+  for root, dirs, files in os.walk(first_inp_directory):
     for file in files:
-      if file.endswith('.inp'):
+      if file.endswith('.inp'):#hypothesis that there is only one inp file per step directory
         inp_file_name=os.path.splitext(os.path.basename(file))[0] #ccx only needs the filename, not the extension
+        inp_file_path=os.path.join(first_inp_directory,file)
+        break
+    else:
+      continue
+    break
   os.system(ccx_exe_path+" "+inp_file_name)
-  #monitor=""
-  #while "Job Finished" not in monitor and "*ERROR" not in monitor: #need to add an error check, in case there're issue with the inp, rin files, or anything else
-  #  monitor=sys.stdout
+  
+  #Need to build and add a monitor to check if there are no issue with the files, with the subprocess module function, and continue only if the previous step has been completed
+  rout_file_dir=first_inp_directory
+  step_dir=first_inp_directory
+  [Disp_node_set_name,first_degree_freedom,last_degree_freedom]=get_disp_characteristics(inp_file_path)
+  for i in range(2,total_step+1):
+    #send the outputs for new input calc in the other FMU
+    [mass_matrix, stiff_matrix, disp_values]=read_and_send_outputs(step_dir)
+    
+    #update the FMU inputs, with those received from the other FMU
+    [first_increment_value,step_duration,min_increment_value,max_increment_value,new_step_name,output_type,new_disp_value]=update_inputs(other_fmu)
+    
+    new_step_folder_name="Step_"+i
+    new_step_name="init_Step_"+i
+    
+    #Procedure for the *RESTART function, check ccx manual for more info
+    step_dir=copy_rename_rout_to_rin(work_dir,rout_file_dir,new_step_folder_name, new_step_name)
+    
+    #Generate and run the new step inp
+    write_new_step_inpfile_with_restart_read_write(first_increment_value,step_duration,min_increment_value,max_increment_value,new_step_name,output_type)
+    run_inp_file(ccx_exe_path, step_dir, new_step_name)
 
   
-def copy_rename_rout_to_rin(new_step_folder_name, rout_file_dir,new_step_name):
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+def read_and_send_outputs(step_dir):
+  return mass_matrix, stiff_matrix, disp_values
+
+def update_inputs(other_fmu):
+  return first_increment_value,step_duration,min_increment_value,max_increment_value,new_step_name,output_type,new_disp_value
+  
+def copy_rename_rout_to_rin(work_dir,rout_file_dir,new_step_folder_name, new_step_name):
   # New step folder path
-  new_path = os.path.join(rout_file_dir, new_step_folder_name)
+  new_path = os.path.join(work_dir, new_step_folder_name)
   # Create the directory
   try:
     os.mkdir(new_path)
@@ -32,18 +71,55 @@ def copy_rename_rout_to_rin(new_step_folder_name, rout_file_dir,new_step_name):
         continue
       break
     rout_path = os.path.join(rout_file_dir, rout_file_name)#build the complete path for the rout file
-    shutil.copy(rout_file_dir,new_path) #copy the file to the new folder
+    shutil.copy(rout_path,new_path) #copy the file to the new folder
 
     copied_rout_file = os.path.join(new_path,rout_file_name)#build the new path for the rout file
     new_rin_file_name = os.path.join(new_path, new_step_name+".rin")#define the new step file name, this name must be the same as the inp file, here new_step_name 
 
     os.rename(copied_rout_file, new_rin_file_name)#rename
+    return new_path
   except OSError as error:
     print(error)
 
 
+def write_new_step_inpfile_with_restart_read_write(first_increment_value,step_duration,min_increment_value,max_increment_value,new_step_name,output_type):#needs a previous run, rename the last_step.rout into new_inp_file.rin 
+	
+  new_inp=open(new_step_name+".inp", 'w')
+	#Continuing the previous step calculation
+	new_inp.write("*RESTART, READ\n")
+	
+	#Step characteristics and analysis type
+	new_inp.write("*STEP, INC=1000000\n")
+	new_inp.write("*DYNAMIC\n")
+	new_inp.write(str(first_increment_value)+","+str(step_duration)+","+str(min_increment_value)+","+str(max_increment_value)+"\n")
+	
+	#Saving the calculation for next step
+	new_inp.write("*RESTART, WRITE\n")
+	
+	#Displaced nodes characteristics 
+	new_inp.write("*BOUNDARY\n")
+	new_inp.write(Disp_node_set_name+","+str(first_degree_freedom)+","+str(last_degree_freedom)+","+str(disp_value)+"\n")
+
+	#Fixed nodes
+	new_inp.write("*BOUNDARY\n")
+	new_inp.write(Fixed_node_set_name+",1,6,0\n")
+
+	#Output files and values
+	new_inp.write("*NODE PRINT, NSET="+Disp_node_set_name)
+	if output_type=="Disp":
+		new_inp.write("\nU\n")
+	elif output_type=="Force":
+		new_inp.write(",Totals=Only\nRF\n")
+		
+	new_inp.write("*END STEP")
+	new_inp.close()
+
+def run_inp_file(ccx_exe_path, step_dir, new_step_name):
+  os.chdir(step_dir)
+  os.system(ccx_exe_path+" "+new_step_name)
   
   
+
   
 
   
