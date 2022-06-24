12cb0209eaff3f0fefc10a3023ddfbadaab64141
MarcusAdielsonGuichet <106607019+MarcusAdielsonGuichet@users.noreply.github.com>
2022-06-24 10:20:16 +0200

    Update add_step.py
    merging the new inp file writer to the run py class/py code

--- Python Scripts/add_step.py
+++ Python Scripts/add_step.py
@@ -23,38 +23,3 @@ def add_step(inp_file):
 		new_inp.write(line)
 	new_inp.close()
 	previous_inp.close()
-
-def write_new_step_inpfile_with_restart_read_write(last_step_rout_path,output_type):#needs a previous run, rename the last_step.rout into new_inp_file.rin 
-	
-	#Renaming the previous step rout into the rin for the next step, as part of the 
-	os.rename(last_step_rout_path,'new_inp_file.rin')
-	
-	new_inp=open("new_inp_file.inp", 'w')
-	#Continuing the previous step calculation
-	new_inp.write("*RESTART, READ\n")
-	
-	#Step characteristics and analysis type
-	new_inp.write("*STEP, INC=1000000\n")
-	new_inp.write("*DYNAMIC\n")
-	new_inp.write(str(first_increment_value)+","+str(step_duration)+","+str(min_increment_value)+","+str(max_increment_value)+"\n")
-	
-	#Saving the calculation for next step
-	new_inp.write("*RESTART, WRITE\n")
-	
-	#Displaced nodes characteristics 
-	new_inp.write("*BOUNDARY\n")
-	new_inp.write(Disp_node_set_name+","+str(first_degree_freedom)+","+str(last_degree_freedom)+","+str(disp_value)+"\n")
-
-	#Fixed nodes
-	new_inp.write("*BOUNDARY\n")
-	new_inp.write(Fixed_node_set_name+",1,6,0\n")
-
-	#Output files and values
-	new_inp.write("*NODE PRINT, NSET="+Disp_node_set_name)
-	if output_type=="Disp":
-		new_inp.write("\nU\n")
-	elif output_type=="Force":
-		new_inp.write(",Totals=Only\nRF\n")
-		
-	new_inp.write("*END STEP")
-	new_inp.close()
