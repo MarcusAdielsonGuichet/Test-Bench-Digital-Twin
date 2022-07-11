9cf46011f21633249de2eced7de74d60c1250c2a
Your Name <marcus.adielson-guichet@mines-ales.org>
2022-07-11 12:10:43 +0200

    Adding output getters

--- example_unifmu_standalone/cantilever/resources/model.py
+++ example_unifmu_standalone/cantilever/resources/model.py
@@ -339,6 +339,41 @@ class Model:
         file.close()#is this necessary?
         return result_forces_values
 
+    def get_mass_matrix(mas_filename):
+        file=open(mas_filename,'r')
+        result_mat=[]
+        for line in file:
+            if len(line.split())>1:#filters out the last blank line
+                mat_line, mat_column,value=int(line.split()[0]),int(line.split()[1]),float(line.split()[2])
+                result_mat.append([mat_line,mat_column,value])
+
+        #Storing the values into a np array? Given that only non-zero values are given by the .mas file, is it important to have the complete matrix?
+        n,p= result_mat[len(result_mat)-1][0],result_mat[len(result_mat)-1][1]
+        mass_mat=np.zeros((n,p))
+        for element in result_mat:
+            mass_mat[element[0]-1,element[1]-1]=element[2]
+
+        file.close()#is this necessary?
+        return mass_mat
+
+    def get_stiffness_matrix(sti_filename):
+        file=open(sti_filename,'r')
+        result_mat=[]
+        for line in file:
+            if len(line.split())>1:
+                mat_line, mat_column,value=int(line.split()[0]),int(line.split()[1]),float(line.split()[2])
+                result_mat.append([mat_line,mat_column,value])
+
+        #Storing the values into a np array? Given that only non-zero values are given by the .sti file, is it important to have the complete matrix? Besides 4 digits are lost in conversion apparently
+        n,p= result_mat[len(result_mat)-1][0],result_mat[len(result_mat)-1][1]
+        stif_mat=np.zeros((n,p))
+        for element in result_mat:
+            stif_mat[element[0]-1,element[1]-1]=element[2]
+
+        file.close()#is this necessary?
+        return stif_mat
+
+
 
 class Fmi2Status:
     """Represents the status of the FMU or the results of function calls.
