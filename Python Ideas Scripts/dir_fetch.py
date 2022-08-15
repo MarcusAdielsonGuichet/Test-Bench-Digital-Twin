b6dde0d2a04f052153dcc5b0e5a2261603354c29
Your Name <marcus.adielson-guichet@mines-ales.org>
2022-08-15 14:33:09 +0200

    Rearanging rep

--- Python Ideas Scripts/dir_fetch.py
+++ Python Ideas Scripts/dir_fetch.py
@@ -1,3 +1,10 @@
 from pathlib import Path
-def get_ccx_path():
-    print(Path.cwd())
+import os
+
+current_path=Path(__file__).resolve()#repr() reads the string as raw characters
+folders=os.path.split(current_path)[0]
+os.chdir(folders)
+print(folders)
+ccx_path="./../CCX Files/calculix2.19win64/ccx/ccx_219.exe"
+os.chdir(ccx_path)
+print(os.getcwd())
